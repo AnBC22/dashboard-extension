e4c2c66b3bd3dc94f7c4c39a932b505092bb9429
AnBC22 <104400577+AnBC22@users.noreply.github.com>
2024-01-05 15:24:22 -0600

    Layout improvement
    Image quality improved and increased font-sizes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 fetch("https://apis.scrimba.com/unsplash/photos/random?orientation=landscape&query=nature")
     .then(res => res.json())
     .then(data => {
-        document.body.style.backgroundImage = `url(${data.urls.regular})`
+        document.body.style.backgroundImage = `url(${data.urls.full})`
 		document.getElementById("author").textContent = `By: ${data.user.name}`
     })
     .catch(err => {
-        // Use a default background image/author
+        // This is to use a default background image/author
         document.body.style.backgroundImage = `url(https://images.unsplash.com/photo-1560008511-11c63416e52d?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyMTEwMjl8MHwxfHJhbmRvbXx8fHx8fHx8fDE2MjI4NDIxMTc&ixlib=rb-1.2.1&q=80&w=1080
 )`
 		document.getElementById("author").textContent = `By: Dodi Achmad`
     })
 
-fetch("https://api.coingecko.com/api/v3/coins/dogecoin")
+fetch("https://api.coingecko.com/api/v3/coins/bitcoin")
     .then(res => {
         if (!res.ok) {
             throw Error("Something went wrong")
@@ -38,8 +38,12 @@ function getCurrentTime() {
 
 setInterval(getCurrentTime, 1000)
 
+function error(err) {
+    console.warn(`ERROR(${err.code}): ${err.message}`);
+  }
+
 navigator.geolocation.getCurrentPosition(position => {
-    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=imperial`)
+    fetch(`https://apis.scrimba.com/openweathermap/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&units=metric`)
         .then(res => {
             if (!res.ok) {
                 throw Error("Weather data not available")
@@ -55,4 +59,8 @@ navigator.geolocation.getCurrentPosition(position => {
             `
         })
         .catch(err => console.error(err))
-});
+}, error, {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0,
+  });
